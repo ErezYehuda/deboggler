844dd6eb87b83f0887c3f27de5580b51a5d719a7
ErezYehuda <harrisjweinstein@gmail.com>
2020-06-14 23:39:43 -0400

    Load and decompress compressed Scrabble dictionary

--- js/post_dictionary.js
+++
@@ -1,8 +0,0 @@
-var dictionary_root = build_dictionary_tree(scrabble_dictionary);
-
-app.letters = (
-            "AHEL" +
-            "BDSM" +
-            "DETE" +
-            "QEEN"
-        );
